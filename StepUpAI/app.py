24b221234c3af496f9598b4a1095464dbf6e4a3b
adarshlearnngrow <adarsh.learnandgrow@gmail.com>
2025-05-17 20:25:53 +0100

    Update app.py

--- StepUpAI/app.py
+++ StepUpAI/app.py
@@ -267,11 +267,13 @@ def generate_hybrid_action_plan(tech, soft, trans, skill_resources):
 
         return plan
 
-            
+import os
+print("Working directory:", os.getcwd())
+print("Files in ./models:", os.listdir("models"))           
 # Load Mentor Clustering Model & Vectorizer
-with open('models//mentor_clustering_model.pkl', 'rb') as f:
+with open('models/mentor_clustering_model.pkl', 'rb') as f:
         kmeans_final = joblib.load(f)
-with open('models//fitted_vectorizer.pkl', 'rb') as f:
+with open('models/fitted_vectorizer.pkl', 'rb') as f:
     vectorizer = joblib.load(f)
 mentors_final_data = pd.read_json("mentors_final_data.json")
 
